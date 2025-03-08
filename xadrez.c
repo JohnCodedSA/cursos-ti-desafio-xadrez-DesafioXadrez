f4380a1c17f3e53d9865b29d5471cfe99655581e
Johnatan Silva de Araujo <karasutsurj@gmail.com>
2025-03-08 14:56:25 +0000

    Desafio Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -17,15 +17,30 @@ int main(){
         j++;
     }
 
-    //Movimento da Rainha (8 casas para esquerda)
-    printf("\nMovimento da Rainha\n");
+    // Movimento da Rainha (8 casas para esquerda)
+    printf("\nMovimento da Rainha:\n");
     int k = 0;
     do{
         printf("Esquerda\n");
         k++;
     }while(k < 8);
-    
 
+    // Movimento do Cavalo (2 casas para baixo e 1 para esquerda)
+    printf("\nMovimento do Cavalo:\n");
+
+    // Primeiro movimento 2 casas para baixo
+    for(int i = 0; i < 2; i++)
+    {
+        printf("Baixo\n");
+    }
+
+    // Segundo movimento 1 casa para esquerda
+    int i = 0;
+    while(i < 1)
+    {
+        printf("Esquerda\n");
+        i++;
+    }
 
     return 0;
 }
